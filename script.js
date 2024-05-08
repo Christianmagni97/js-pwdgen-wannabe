17ce1de7ee9ffcfe3086c72411a067938ae82171
Christianmagni97 <christianmagni28@gmail.com>
2024-05-08 15:35:18 +0200

    commit percorso

--- script.js
+++ script.js
@@ -3,7 +3,4 @@ const userFirstName = prompt('Type your first name' );
 const userLastName = prompt('Type your last name' );
 const userFavColour = prompt('Type your favourite colour' );
 console. log(userFirstName, userLastName, userFavColour);
-8
-9
-10
-document. getElementById('output'). innerHTML = "${userFirstName}${userLastName}${userFavColour}24*;
+
